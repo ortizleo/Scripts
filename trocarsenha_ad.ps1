4b04f7ec33ad97b4d67add85040864392378c436
Leonardo Ortiz <leonardo.ortz@gmail.com>
2021-02-07 23:13:56 -0300

    Create trocarsenha_ad.ps1

---
+++ trocarsenha_ad.ps1
@@ -0,0 +1,34 @@
+# Script pra definir uma senha randomica para usuários de um .csc (coluna "Conta")
+# Função pra definir senha: https://activedirectoryfaq.com/2017/08/creating-individual-random-passwords/
+# Leonardo
+
+Import-Csv -Path “c:\temp\users.csv” | ForEach-Object {
+
+function Get-RandomCharacters($length, $characters) {
+    $random = 1..$length | ForEach-Object { Get-Random -Maximum $characters.length }
+    $private:ofs=""
+    return [String]$characters[$random]
+}
+ 
+function Scramble-String([string]$inputString){     
+    $characterArray = $inputString.ToCharArray()   
+    $scrambledStringArray = $characterArray | Get-Random -Count $characterArray.Length     
+    $outputString = -join $scrambledStringArray
+    return $outputString 
+}
+ 
+$password = Get-RandomCharacters -length 5 -characters 'abcdefghiklmnoprstuvwxyz'
+$password += Get-RandomCharacters -length 3 -characters 'ABCDEFGHKLMNOPRSTUVWXYZ'
+$password += Get-RandomCharacters -length 3 -characters '1234567890'
+$password += Get-RandomCharacters -length 3 -characters '!$%&/()=?}][{@#*+'
+ 
+
+$password = Scramble-String $password
+
+
+
+Write-Host "$_.’Conta’ senha: $password"
+
+Set-ADAccountPassword -Identity $_.’Conta’ -Reset -NewPassword (ConvertTo-SecureString -AsPlainText "$password" -Force)
+
+}
